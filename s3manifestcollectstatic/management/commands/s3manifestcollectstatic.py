17aabc0d9d72ef7c306039669abfea8ed5ea352c
Felix Ippolitov <fellu@ancient.fi>
2023-05-31 13:16:24 +0300

    Support for Django >=4.2
    Closes #2

--- s3manifestcollectstatic/management/commands/s3manifestcollectstatic.py
+++ s3manifestcollectstatic/management/commands/s3manifestcollectstatic.py
@@ -1,12 +1,14 @@
 import json
 from concurrent.futures import ThreadPoolExecutor
+from contextlib import contextmanager
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
+from django import VERSION
 from django.conf import settings
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
-from django.utils.module_loading import import_string
 
 MANIFEST_PATH = "staticfiles.json"
 
@@ -27,32 +29,47 @@ class Command(BaseCommand):
             help="Force the reupload of files",
         )
 
+    @staticmethod
+    @contextmanager
+    def override_storage_settings(static_root, staticfiles_storage):
+        saved_static_root = settings.STATIC_ROOT
+        settings.STATIC_ROOT = static_root
+
+        if VERSION < (4, 2) or settings.is_overridden("STATICFILES_STORAGE"):
+            saved_storage = settings.STATICFILES_STORAGE
+            settings.STATICFILES_STORAGE = staticfiles_storage
+            yield
+            settings.STATICFILES_STORAGE = saved_storage
+        else:
+            saved_storage = settings.STORAGES
+            settings.STORAGES = {
+                **saved_storage,
+                "staticfiles": {
+                    "BACKEND": staticfiles_storage,
+                },
+            }
+            yield
+            settings.STORAGES = saved_storage
+
+        settings.STATIC_ROOT = saved_static_root
+
     def handle(self, *args, **options):
         self.force = options["force"]
         self.verbosity = options["verbosity"]
 
         with TemporaryDirectory() as tmpdirname:
-            saved_static_root = settings.STATIC_ROOT
-            saved_storage = settings.STATICFILES_STORAGE
-
-            settings.STATIC_ROOT = tmpdirname
-            settings.STATICFILES_STORAGE = (
-                "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
-            )
-
-            call_command("collectstatic")
-
-            settings.STATIC_ROOT = saved_static_root
-            settings.STATICFILES_STORAGE = saved_storage
-
-            storage = import_string(saved_storage)()
+            with self.override_storage_settings(
+                tmpdirname,
+                staticfiles_storage="django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+            ):
+                call_command("collectstatic")
 
             manifest = Path(tmpdirname) / MANIFEST_PATH
             with manifest.open("rb") as f:
                 to_upload = set(json.load(f)["paths"].values())
 
-            if storage.exists(MANIFEST_PATH):
-                with storage.open(MANIFEST_PATH) as f:
+            if staticfiles_storage.exists(MANIFEST_PATH):
+                with staticfiles_storage.open(MANIFEST_PATH) as f:
                     already_uploaded = set(json.load(f)["paths"].values())
                     intersection = to_upload.intersection(already_uploaded)
                     self.log(f"{len(intersection)} files were already uploaded", level=1)
@@ -65,7 +82,7 @@ class Command(BaseCommand):
             def _save_asset(path):
                 path_obj = Path(tmpdirname) / path
                 with path_obj.open("rb") as f:
-                    storage.save(path, f)
+                    staticfiles_storage.save(path, f)
                 return path
 
             self.log(f"Start the upload of {len(to_upload)} files", level=1)
